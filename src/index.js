4e6116e2e7a5e8a22025028f96c0db32e4b43b56
Unknown <prpr48@naver.com>
2020-02-26 10:08:42 +0900

    add schema.graphql

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
-// 1
 let links = [
   {
     id: 'link-0',
@@ -25,10 +11,8 @@ let links = [
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    // 2
     feed: () => links
   },
-  // 3
   Link: {
     id: parent => parent.id,
     description: parent => parent.description,
@@ -37,7 +21,7 @@ const resolvers = {
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 });
 
